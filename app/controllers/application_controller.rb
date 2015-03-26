90eb105079b29296eae4c93f8fb4625f5af23629
Shaun Martin <webmaster@ginblades.com>
2015-03-26 08:52:32 -0500

    Using native datetime-local
    Instead of datetimepicker gem. Looks good on chrome and iOS,
    Not great in Firefox or IE11.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  Date::DATE_FORMATS[:default] = "%Y/%m/%d"
-  Time::DATE_FORMATS[:default] = "%Y/%m/%d %I:%M%p"
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       [base_title, page_title].join( ' | ')
     end
   end
+
+  def date_form(time)
+    time.try(:strftime, '%Y-%m-%d')
+  end
+
+  def time_form(time)
+    time.try(:strftime, '%Y-%m-%dT%H:%M:00')
+  end
 end
