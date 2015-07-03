5437970b89760b79d6c2da0fbdd7424c1c6f61f2
Shaun Martin <webmaster@ginblades.com>
2015-07-03 07:13:19 -0500

    Some simple fixes.

--- app/controllers/admin/dictionary/dashboard_controller.rb
+++ app/controllers/admin/dictionary/dashboard_controller.rb
@@ -1,4 +1,4 @@
-class Admin::Dictionary::DashboardController < ApplicationController
+class Admin::Dictionary::DashboardController < AdminController
   def index
     @entries = current_user.dictionary_entries.includes(:source, :target).order(created_at: :desc).limit(10)
     @languages = ::Dictionary::Language.where.not(entry_counter: 0).order(name: :asc)
--- app/controllers/admin/health/dashboard_controller.rb
+++ app/controllers/admin/health/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::Health::DashboardController < AdminController
   def index
+    @health_entries = ::Health::Entry.order(entry_date: :desc)
+    @health_meals = ::Health::Meal.order(time: :desc)
+    @health_workouts = ::Health::Workout.order(start: :desc)
   end
 end
